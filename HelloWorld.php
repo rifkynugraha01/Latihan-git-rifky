323c321b610acaabc9381d63e2c0a524a4dd7831
rifkynugraha01 <rifkyn897@gmail.com>
2026-05-21 13:23:02 +0700

    first commit

---
+++ HelloWorld.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    // $a=14;
+    // $b=12;
+    // $c=$a+$b;
+    // echo $c;
+
+    // $nilai1=26;
+    // $nilai2=25;
+    // $hasil=$nilai1%$nilai2;
+    // echo $hasil;
+
+    // var_dump(True && True);
+    // echo"<br>";
+    // Var_dump(True && false);
+    // echo"<br>";
+    // Var_dump(false && false);
+    // echo"<br>";
+    // Var_dump(True && false);
+
+    // for($i=1;$i<=5;$i++) {
+    //   echo $i;
+    //   echo("<br>");
+    // }
+
+    // for($i=5;$>=1;$i--)
+    // echo ($i);
+    // echo("<br>");
+
+  //   for($i=5;$i>=1;$i++) {
+  //      for($j=5;$j>=1;$j++) {
+  //          echo"$j";
+  //      }
+  //    echo"<br>";
+  //  }
+
+  // $i=1;
+  //     for($i=1;$i <=5; $i++){
+  //       for($j=1; $j <=5; $j++){
+  //         echo($j);
+  //       }
+  //       echo"<br>";
+  //     }
+  //       echo"<br>";
+  //     for($i=5;$i >=1; $i--){
+  //       for($a=5; $a >=1; $a--){
+  //         echo($i);
+  //       }
+  //       echo"<br>";
+  //     }
+  //       echo"<br>";
+
+  //       echo"<br>";
+  //     for($i=1; $i <=5; $i++){
+  //       for($j=5; $j>=$i;$j--){
+  //         echo($j);
+  //       }
+  //         echo"<br>";
+  //     }
+         
+
+      // for($i=1; $i <=5; $i++){
+      //   if($i %2==1){
+      //     $angka=0
+      //   else{
+      //     $angka=1
+      //   }
+      // for($j=1; $j <=$i; $j++){
+      //     echo($angka);
+      // }
+      //     echo"<br>";
+      //   }
+      // }
+
+        
+          //  fungsion (default parameter)
+      // function sayhello($name){
+      //   echo("hello $name");
+      // }
+      // sayhello("Ahmad");
+
+      // function sayhello ($firtname, $lastname="Ahmad"){
+      //   echo ("hello $firtname $lastname");
+      // }
+      //   sayhello("ari");
+
+        // Return
+      function cetakhello ($nama){
+        echo ("hallo $nama");
+      }
+      $hasil= cetakhello ("arie");
+        echo $hasil;
+        echo strtoupper ($hasil);
+
+      
+  ?>
+</body>
